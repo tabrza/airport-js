f44a5245df75c5c88041c923e7d41064ffa3e414
Tabish Raza <tabishraza@gmail.com>
2017-11-21 17:00:57 +0000

    test for not landing plane when terminal at capacity

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,9 +1,14 @@
 describe('Airport', function(){
   var airport;
+  var plane;
+  var plane2;
+  var plane3;
 
   beforeEach(function(){
     airport = new Airport();
     plane = new Plane();
+    plane2 = new Plane();
+    plane3 = new Plane();
   });
 
   describe('when landing a plane', function(){
@@ -25,6 +30,15 @@ describe('Airport', function(){
       airport.landPlane(plane);
       var landedPlane = airport.terminal[airport.terminal.length - 1];
       expect(landedPlane).toBe(undefined);
+    });
+
+    it('does not land if terminal at full capacity', function(){
+      plane.isFlying = true;
+      airport.weather = 'clear';
+      airport.landPlane(plane);
+      airport.landPlane(plane2);
+      airport.landPlane(plane3);
+      expect(airport.terminal).toEqual([plane, plane2]);
     });
   });
 
