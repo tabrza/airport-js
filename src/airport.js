e802de7b76d14eabfaf180eecb397272a1212ea1
Tabish Raza <tabishraza@gmail.com>
2017-11-21 15:42:20 +0000

    landed plane added to terminal

--- src/airport.js
+++ src/airport.js
@@ -1,8 +1,10 @@
 function Airport(){
-
+  this.terminal = [];
 };
 
 
 Airport.prototype.landPlane = function(plane){
+  this.terminal.push(plane);
+  console.log(this.terminal);
   return true;
 };
