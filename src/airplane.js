a5531649b53d194242b9587961bda76817083e57
Tabish Raza <tabishraza@gmail.com>
2017-11-22 12:17:46 +0000

    setting up of plane function

--- src/airplane.js
+++ src/airplane.js
@@ -1,3 +1,11 @@
 function Plane(){
   this.isFlying = true;
 };
+
+Plane.prototype.landed = function(){
+  return plane.isFlying = false;
+}
+
+Plane.prototype.takeOff = function(){
+  return plane.isFlying = true;
+}
