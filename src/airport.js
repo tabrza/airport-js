30ebfaca21bc055acd3f2e4958fa1327379f4059
Tabish Raza <tabishraza@gmail.com>
2017-11-22 12:18:28 +0000

    calling random weather stormy function

--- src/airport.js
+++ src/airport.js
@@ -1,19 +1,19 @@
 const maxCapacity = 2;
-function Airport(){
+function Airport(weather = new Weather()){
   this.terminal = [];
-  this.weather = 'clear';
+  this.weather = weather;
 };
 
 
 Airport.prototype.landPlane = function(plane){
-  if(this.weather === 'clear' && this.terminal.length < maxCapacity){
+  if(this.weather.isStormy() === false && this.terminal.length < maxCapacity){
     this.terminal.push(plane);
     return true;
   }
 };
 
 Airport.prototype.departPlane = function(plane){
-  if(this.weather === 'clear'){
+  if(this.weather.isStormy() === false){
     for(var i=0; i < this.terminal.length; i++){
       if(this.terminal[i] === plane){
         this.terminal.splice(i,1);
