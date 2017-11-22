340f37e8183e592be12875c47fd3c2fddc60bbaa
Tabish Raza <tabishraza@gmail.com>
2017-11-22 12:16:41 +0000

    stubbing weather into tests

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -3,9 +3,11 @@ describe('Airport', function(){
   var plane;
   var plane2;
   var plane3;
+  var weather;
 
   beforeEach(function(){
-    airport = new Airport();
+    weather = jasmine.createSpyObj('Weather', ['isStormy']);
+    airport = new Airport(weather);
     plane = new Plane();
     plane2 = new Plane();
     plane3 = new Plane();
@@ -13,20 +15,28 @@ describe('Airport', function(){
 
   describe('when landing a plane', function(){
     it('instruct the plane to land', function(){
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       plane.isFlying = true;
       expect(airport.landPlane(plane)).toBe(true);
     });
 
     it('confirms plane is in terminal', function(){
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       plane.isFlying = true;
       airport.landPlane(plane);
       var landedPlane = airport.terminal[airport.terminal.length - 1];
       expect(landedPlane).toEqual(plane);
     });
 
-    it('does not land plane if not clear', function(){
+    it('does not land plane if stormy', function(){
       plane.isFlying = true;
-      airport.weather = 'not clear';
+      weather.isStormy.and.callFake(function(){
+        return true;
+      });
       airport.landPlane(plane);
       var landedPlane = airport.terminal[airport.terminal.length - 1];
       expect(landedPlane).toBe(undefined);
@@ -34,7 +44,9 @@ describe('Airport', function(){
 
     it('does not land if terminal at full capacity', function(){
       plane.isFlying = true;
-      airport.weather = 'clear';
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       airport.landPlane(plane);
       airport.landPlane(plane2);
       airport.landPlane(plane3);
@@ -44,11 +56,17 @@ describe('Airport', function(){
 
   describe('when plane is taking off', function(){
     it('instructs a plane to take off', function(){
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       plane.isFlying = false;
       expect(airport.departPlane(plane)).toBe(true);
     });
 
     it('confirms plane is no longer in terminal', function(){
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       plane.isFlying = true;
       airport.landPlane(plane);
       airport.departPlane(plane);
@@ -56,9 +74,14 @@ describe('Airport', function(){
     });
 
     it('does not takeoff if weather is stormy', function(){
+      weather.isStormy.and.callFake(function(){
+        return false;
+      });
       plane.isFlying = true;
       airport.landPlane(plane);
-      airport.weather = 'not clear'
+      weather.isStormy.and.callFake(function(){
+        return true;
+      });
       airport.departPlane(plane);
       expect(airport.terminal).toEqual([plane]);
     });
